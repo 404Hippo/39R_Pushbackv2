c4885a1bfe5e853f6203dce89e4a14d7ce7615f3
Edward Chang <beiccreeper@gmail.com>
2026-04-17 18:17:25 -0400

    a lot of auto updating. rerouted countersplit, added soloawpsplit, and shortened delays

--- include/autons.h
+++ include/autons.h
@@ -3,11 +3,14 @@
 #include "main.h"
 
 extern void soloAWP();
+extern void soloAWPSplit();
 extern void leftSplit();
 extern void rightSplit();
 extern void leftRush();
 extern void rightRush();
-extern void leftCounterSplit();
+extern void leftCounterSplitOLD();
+extern void rightCounterSplitOLD();
 extern void rightCounterSplit();
+extern void leftCounterSplit();
 extern void skills();
 extern void test();
